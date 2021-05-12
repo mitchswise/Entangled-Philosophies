4856a7dfcb767059548b19c907b77c0df93f7b3a
Blaze Wiseman <kawaiimicrowaves930@gmail.com>
2021-05-12 17:25:57 -0400

    change

--- entangled/src/api.js
+++ entangled/src/api.js
@@ -15,15 +15,12 @@ export function testConnect() {
 
     connect("POST", url);
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, false);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
     var jsonPayload = "";
 
     try {
 		xhr.send(jsonPayload);
 		var jsonObject = JSON.parse(xhr.responseText);
+		return jsonObject;
     } catch (err) {
         return null;
     }
