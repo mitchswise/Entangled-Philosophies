030e3b296a7ecc1464be60b89f8f44437a3d3fd6
Ahmad Barhamje <ahmadbarhamje@hotmail.com>
2021-05-13 11:21:30 -0400

    fixed login endpoint

--- api/login.php
+++ api/login.php
@@ -9,38 +9,38 @@
 	$_username = $inputData["username"];
 	$_password = $inputData["password"];
 	
-	$conn = mysqli_connect($host, $user, $password, $dbname);
+	$conn = mysqli_connect($host, $username, $password, $dbname);
 	if($conn->connect_error) {
 		die("Connection failed " . $conn->connect_error);
 	}
 	
-	$query = "SELECT username, password, id FROM users WHERE username = '" . $_username . "';";
+	$query = "SELECT username, password, id, is_verified FROM users WHERE username = '" . $_username . "';";
 	$result = mysqli_query($conn, $query);
-	$error = false;
+
+	$error_code = 0;
 	$user_id = -1;
 	
 	if($result->num_rows > 0) {
-	#if(true)
 		$row = $result->fetch_assoc();
-		
 		#if(password_verify($_password, $row["password"])) {
-		#	$user_id = $row["id"];
-		#}
 		if($_password == $row["password"]) {
 			$user_id = $row["id"];
-			$user_id = 3;
+			if($row["is_verified"] == 0) {
+				$error_code = 2;
+			}
 		}
 		else {
-			$error = true;
-			$user_id = 2;
+			$error_code = 1;
 		}
 	}
 	else {
-		$error = true;
-		$user_id = 1;
+		$error_code = 1;
 	}
 	
-	$ret_value = '{"UserID": ' . $user_id . ', "username": ' . $_username . '}';
+	$error_message = "";
+	if($error_code == 1) $error_message = "Username/Password combination does not exist.";
+	else if($error_code == 2) $error_message = "Account not activated";
+
+	$ret_value = '{"UserID": ' . $user_id . ', "error_code": ' . $error_code . ', "error_message": "' . $error_message . '"}';
 	echo $ret_value;
-	
 ?>
