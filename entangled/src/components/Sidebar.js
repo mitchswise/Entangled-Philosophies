8353c77707d1ed36dee0689cb43527f7a6dbf0f2
AhmadBarhamje <ahmadbarhamje@hotmail.com>
2021-05-19 18:46:59 -0400

    added logout feature to sidebar

--- entangled/src/components/Sidebar.js
+++ entangled/src/components/Sidebar.js
@@ -1,8 +1,13 @@
   
 import React from 'react';
-import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 import './Sidebar.css';
+import { cookies } from '../api';
+
+function logout() {
+  cookies.remove('UserID', { path: '/' });
+}
 
 export default props => {
   return (
@@ -17,10 +22,13 @@ export default props => {
         <Link to="/login">Login</Link>
       </a>
       <a className="menu-item">
-        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </a>
       <a className="menu-item">
 		    <Link to="/admin">Admin</Link>
+	    </a>
+      <a className="menu-item">
+		    <Link to="/login" onClick={logout} >Logout</Link>
 	    </a>
     </Menu>
   );
