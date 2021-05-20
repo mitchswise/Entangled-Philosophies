0b5fc5f0af8a8a4f0ad7d91730377f4cbc54bec7
Ahmad Barhamje <ahmadbarhamje@hotmail.com>
2021-05-20 14:27:01 -0400

    handles correct category

--- api/addTag.php
+++ api/addTag.php
@@ -2,6 +2,7 @@
 	header("Access-Control-Allow-Headers: Content-type");
 	header("Access-Control-Allow-Origin: *");
 	include "database.php";
+    include "languages.php";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
@@ -16,9 +17,14 @@
     $category_name = $inData["category"];
     $category_id = -1;
 
-    $query = "SELECT category_id FROM category_translation WHERE language = '" . $language . "' AND text = '" . $category_name . "'
-            AND owner = " . $userID . ";";
+    $query = "SELECT category_id FROM category_translation WHERE language = '" . $language . "' AND text = '" . $category_name . "' AND owner = " . $userID . ";";
     $result = $conn->query($query);
+    if(!$result) {
+        $message = '{"status":"' . $conn->error . '"}';
+        echo $message;
+        return;
+    }
+
     if($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $category_id = $row["category_id"];
@@ -29,7 +35,21 @@
         return;
     }
 
-    $message = '{"status":"success"}';
+    //now we have a category id to assign the tag to.
+    //check if there are any name conflicts with this tag.
+
+    if($userID == 0) { //Admin tag
+
+    }
+    else { //User tag
+
+    }
+
+    $query = "INSERT INTO tags (owner_id, category_id) VALUES (" . $userID . ", " . $category_id . ");";
+    $result = $conn->query($query);
+    $tag_id = $conn->insert_id;
+    
+    $message = '{"status":"' . $tag_id . '"}';
     echo $message;
 
 ?>
