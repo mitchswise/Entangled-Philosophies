c7d8753d43802693d09747e972cb7ed6ab63f37a
Blaze Wiseman <kawaiimicrowaves930@gmail.com>
2021-05-21 14:20:52 -0400

    get/set settings endpoints

--- api/updateSettings.php
+++ api/updateSettings.php
@@ -5,20 +5,22 @@
 	
 	$inputData = json_decode(file_get_contents('php://input'), true);
 	
-	$user = $inputData["username"];
-	$perms = $inputData["permission_level"];
+	$id = $inputData["id"];
+	$email = $inputData["email"];
+	$language = $inputData["language"];
+	$cookies = $inputData["cookies"];
 	
 	$conn = mysqli_connect($host, $username, $password, $dbname);
 	if($conn->connect_error) {
 		die("Connection failed " . $conn->connect_error);
 	}
 	
-	$query = "UPDATE users SET permission_level = " . $perms . "  WHERE username = '" . $user . "';";
+	$query = "UPDATE users SET email = '" . $email . "', language = '" . $language . "', cookies = " . $cookies . " WHERE id = " . $id . " ;";
 	$result = mysqli_query($conn, $query);
 
 	if(mysqli_affected_rows($conn) > 0) {
 		echo '{"status":"success"}';
 	} else {
-		echo '{"status":"error"}';
+		echo '{"status":"' . $conn->error . '"}';
 	}
 ?>
