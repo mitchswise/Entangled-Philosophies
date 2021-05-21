bc2e47c18f289d26fbbb53a77df7bf5e7f832e2b
AhmadBarhamje <ahmadbarhamje@hotmail.com>
2021-05-20 23:21:12 -0400

    Ahmad add tag (#11)
    * base template for addTag endpoint
    
    * push to move to local machine
    
    * handles correct category
    
    * updated add tag to catch any duplicate tag names
    
    * successfully adds a tag
    
    * users can make tags in admin/their own category
    
    * added testing for add tag endpoint
    
    * commit
    
    * Update documentation.md
    
    * Update documentation.md
    
    * Update documentation.md
    
    * Update documentation.md
    
    * Update documentation.md
    
    * Update documentation.md
    
    * Update documentation.md

---
+++ api/addTag.php
@@ -0,0 +1,121 @@
+<?php
+    header("Access-Control-Allow-Headers: Content-type");
+    header("Access-Control-Allow-Origin: *");
+    include "database.php";
+    include "languages.php";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+    $inData = json_decode(file_get_contents('php://input'), true);
+    $userID = $inData["userID"];
+    $language = $inData["language"];
+
+    //check for invalid category
+    $category_name = $inData["category"];
+    $category_id = -1;
+
+    //admins can only use admin-made categories but users can
+    //use both admin and their own categories.
+    //If there are duplicate category names, we'll prioritize a user's category first.
+    
+    if($userID > 0) {
+        $query = "SELECT category_id FROM category_translation WHERE language = 'def' AND text = '" . 
+                $category_name . "' AND owner = " . $userID . ";";    
+        $result = $conn->query($query);
+        if($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $category_id = $row["category_id"];
+        }
+    }
+    if($category_id == -1) {
+        $query = "SELECT category_id FROM category_translation WHERE language = '" . $language . "' AND text = '" . 
+                    $category_name . "' AND owner = 0;";
+        $result = $conn->query($query);
+    
+        if($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $category_id = $row["category_id"];
+        }
+    }
+
+    if($category_id == -1) {
+        $message = '{"status":"category does not exist."}';
+        echo $message;
+        return;
+    }
+
+    //now we have a category id to assign the tag to.
+    //check if there are any name conflicts with this tag.
+
+    $duplicate_tag = FALSE;
+    $lang_len = count(supported_languages); //# of supported languages
+
+    if($userID == 0) { //Admin tag
+
+        //admins can't have any overlap for each of the supported languages
+        $query = "SELECT * FROM tags_translation WHERE ";
+        for($idx = 0; $idx < $lang_len; $idx++) {
+            if($idx > 0) $query = $query . " OR ";
+            $cur_lang = supported_languages[$idx];
+            $query = $query . "(text = '" . $inData[$cur_lang] . "' AND language = '" . 
+		        $cur_lang . "' AND owner = 0)";
+        }
+        $query = $query . ";";
+
+        $result = $conn->query($query);
+        if($result->num_rows > 0) {
+            $duplicate_tag = TRUE;
+        }
+    }
+    else { //User tag
+        $tag_name = $inData["def"];
+
+        //users cant have overlap with any of their own tags
+        $query = "SELECT * FROM tags_translation WHERE text = '" . $tag_name . "' AND owner = " . $userID . ";";
+        $result = $conn->query($query);
+
+        if($result->num_rows > 0) {
+            $duplicate_tag = TRUE;
+        }
+    }
+
+    if($duplicate_tag == TRUE) {
+        echo '{"status":"duplicate tag name."}';
+        return;
+    }
+
+    //now insert the tag 
+    $query = "INSERT INTO tags (owner_id, category_id) VALUES (" . $userID . ", " . $category_id . ");";
+    $result = $conn->query($query);
+    $tag_id = $conn->insert_id;
+
+    //now insert all translations of the tag
+    if($userID == 0) { //Admin inserts all translations
+        $query = "INSERT INTO tags_translation (tag_id, language, text, owner) VALUES ";
+        for($idx = 0; $idx < $lang_len; $idx++) {
+            if($idx > 0) $query = $query . ", ";
+            $cur_lang = supported_languages[$idx];
+            $add_to_query = "(" . $tag_id . ", '" . $cur_lang . "', '" . $inData[$cur_lang] . "', " . $userID . ")";
+            $query = $query . $add_to_query;
+        }
+        $query = $query . ";";
+    }
+    else { //User inserts just one entry whose langauge is "def".
+        $query = "INSERT INTO tags_translation (tag_id, language, text, owner) VALUES 
+        (" . $tag_id . ", 'def', '" . $inData["def"] . "', " . $userID . ");";
+    }
+
+    $result = $conn->query($query);
+    if(!$result) {
+        $message = '{"status":"' . $conn->error . '", "tagID":"' . $tag_id . '", "SQL":"' . $query . '"}';
+        echo $message;
+        return;
+    }
+    
+    $message = '{"status":"Successfully added tag"}';
+    echo $message;
+
+?>
--- entangled/src/components/AddUser.js
+++ entangled/src/components/AddUser.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {addUser, login, sendActivation, resetPassword} from '../api.js';
+import {addUser, login, sendActivation, resetPassword, supported_languages, addTag} from '../api.js';
 
 function testFunc() {
     var username = document.getElementById("username").value;
@@ -28,25 +28,76 @@ function testResetPassword() {
     document.getElementById("ansField").innerHTML = ("Status: " + data.status);
 }
 
+function testAddTag() {
+    var category = document.getElementById("category").value;
+    if(!category) {
+        document.getElementById("tagStatus").innerHTML = "Please fill out blank sections";
+        return;
+    }
+
+    var jsonLanguages = '{"category":"' + category + '", ';
+    for(const index in supported_languages) {
+        var htmlName = "tagname_"+supported_languages[index];
+        var value = document.getElementById(htmlName).value;
+        if(!value) {
+            document.getElementById("tagStatus").innerHTML = "Please fill out blank sections";
+            return;
+        }
+        jsonLanguages += '"' + supported_languages[index] + '":"' + value + '", ';
+    }
+    jsonLanguages = jsonLanguages.substring(0, jsonLanguages.length - 2) + "}";
+
+    var userID = 0;
+    var language = "eng";
+    var jsonParse = JSON.parse(jsonLanguages);
+
+    var data = addTag(userID, language, jsonParse);
+    document.getElementById("tagStatus").innerHTML = data.status;
+}
+
+function testAddUserTag() {
+    var category = document.getElementById("categoryUser").value;
+    var jsonLanguages = '{"category":"' + category + '", ';
+    var value = document.getElementById("tagname_def").value;
+
+    if(!value || !category) {
+        document.getElementById("tagStatus").innerHTML = "Please fill out blank sections";
+        return;
+    }
+
+    jsonLanguages += '"def":"' + value + '", ';
+    jsonLanguages = jsonLanguages.substring(0, jsonLanguages.length - 2) + "}";
+
+    var userID = 10;
+    var language = "eng";
+    var jsonParse = JSON.parse(jsonLanguages);
+
+    var data = addTag(userID, language, jsonParse);
+    document.getElementById("tagStatus").innerHTML = data.status;
+}
+
 export default class AddUser extends React.Component {
     render() {
         const element = (
         <div className="container" id="outer-container"> 
-            <h1> Hello World!</h1>
-			<input type="text" id="username"/><br/>
-			<input type="text" id="email"/><br/>
-			<input type="text" id="password"/><br/>
-            <button type="button" id="checker" onClick={testFunc}>Test Endpoint</button>
-            <div id="ansField">Status: </div><br/>
-
-			<input type="text" id="loginusername"/><br/>
-			<input type="text" id="loginpassword"/><br/>
-			<button type="button" id="login" onClick={testLogin}>Login</button>
 
             <h3>Forgot Password? Enter username and email</h3>
             <input type="text" id="forgot_username"/><br/>
             <input type="text" id="forgot_email"/><br/>
             <button type="button" id="resetPass" onClick={testResetPassword}>Reset Password</button>
+
+            <h3>Add an Admin tag! Enter category and supply translations in english and german</h3>
+            <input type="text" id="category"/><br/>
+            <input type="text" id="tagname_eng"/><br/>
+            <input type="text" id="tagname_ger"/><br/>
+            <button type="button" id="addTag" onClick={testAddTag}>Add Admin tag</button>
+
+            <h3>Add a User tag! Enter category and the name of your tag</h3>
+            <input type="text" id="categoryUser"/><br/>
+            <input type="text" id="tagname_def"/><br/>
+            <button type="button" id="addTag" onClick={testAddUserTag}>Add User tag</button>
+
+            <div id = "tagStatus">Tag Status: </div>
         </div>
         );
         return element; 
