cfb390a9b8ff474bc2488b68e8725f1dd84c7bf8
AhmadBarhamje <ahmadbarhamje@hotmail.com>
2021-05-24 12:10:28 -0400

    changed things back to the entangled account

--- entangled/src/api.js
+++ entangled/src/api.js
@@ -2,8 +2,7 @@ import Cookies from 'universal-cookie';
 export const cookies = new Cookies();
 export const supported_languages = ["eng", "ger"];
 
-// var urlBase = 'http://chdr.cs.ucf.edu/~entangledPhilosophy/Entangled-Philosophies/api';
-var urlBase = 'http://chdr.cs.ucf.edu/~ah458967/Entangled-Philosophies/api';
+var urlBase = 'http://chdr.cs.ucf.edu/~entangledPhilosophy/Entangled-Philosophies/api';
 
 var xhr;
 
