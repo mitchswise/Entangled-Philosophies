b2930444f9a932b49f238e343983a7cf899407d5
abocreature <abocreature@gmail.com>
2021-05-24 15:06:24 -0400

    UploadPaper hotfix
    oops

--- entangled/src/components/UploadPaper.js
+++ entangled/src/components/UploadPaper.js
@@ -20,8 +20,8 @@ export default class UploadPaper extends React.Component {
 			<div class="dropdown" id="dropdowncontainer2">
 				<button class="dropbtn" id="dropdown2">{tagChoice}</button>
 				<div class="dropdown-content">
-					<button type="submit" onClick=tag1(){ tagChoice="Temp Tag 1"; }>Temp Tag 1</button>
-					<button type="submit" onClick=tag2(){ console.log(tagChoice); }>Temp Tag 2</button>
+					<button type="submit">Temp Tag 1</button>
+					<button type="submit">Temp Tag 2</button>
 					<button type="submit">Temp Tag 3</button>
 					<button type="submit">Temp Tag 4</button>
 					<button type="submit">Temp Tag 5</button>
