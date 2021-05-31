2ba2ccf04ea28217b967adf381810dbcb05027a7
abocreature <abocreature@gmail.com>
2021-05-31 16:15:02 -0400

    initial

--- entangled/src/components/UploadPaper.js
+++ entangled/src/components/UploadPaper.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import './UploadPaper.css';
-import { cookies, addPaper } from '../api.js';
+import { cookies, addPaper, tagExists } from '../api.js';
 
 function doAddPaper() {
 	var title = document.getElementById("titleName").value;
-	var author = document.getElementById("author").value;
+	var author = document.getElementById("authorBox").value;
+	var contributor = document.getElementById("contributor").value;
+	var subject = document.getElementById("subject").value;
+	var date = document.getElementById("date").value;
+	var description = document.getElementById("description").value;
+	var publisher = document.getElementById("publisher").value;
+	var isbn = document.getElementById("isbn").value;
 	var filename = document.getElementById("filename").value;
 	var url;
 
@@ -38,6 +44,32 @@ export default class UploadPaper extends React.Component {
     }
 
     render() {		
+		let tagsAdded = ""
+		let tagsList = []
+		
+		const doAddTag = async e => {
+			var tag = document.getElementById("tagsearch").value;
+			
+			var data = tagExists(tag, "eng", cookies.get('UserID'));
+			console.log(data);
+			
+			if (data.tag_id >= 0)
+			{
+				tagsAdded = tagsAdded + tag + ', ';
+				tagsList.push(tag);
+				document.getElementById("paperStatus").innerHTML = "";
+			}
+			else {
+				document.getElementById("paperStatus").innerHTML = "Tag Not Found";
+			}
+			console.log(tagsList);
+			
+			document.getElementById("tagsearch").value = '';
+			document.getElementById("tags").value = tagsAdded;
+			
+			return;
+		}
+	
         return <div className="container" ref={el => (this.div = el)}>
             <div className="header">
                 <h1 id="title">Upload Paper</h1>
@@ -45,14 +77,103 @@ export default class UploadPaper extends React.Component {
             {this.renderRedirect()}
             <div className="PaperBox">
                 <div className="PaperFields">
+					<br /><div id="addPaperStatus"></div>
                     <h2 id="leftTitle">Title</h2>
-                    <input type="text" className="inputBoxes" id="titleName" /><br />
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="titleName" 
+					/><br />
                     <h2 id="leftAuthor">Author</h2>
-                    <input type="text" className="inputBoxes" id="author" /><br />
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="authorBox"
+					 placeholder="Optional Author"
+					/><br />
+					<h2 id="leftContributor">Contributor</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="contributor"
+					 placeholder="Optional Contributor"
+					/><br />
+					<h2 id="leftRelations">Relation</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="relation"
+					 placeholder="Optional Relations"
+					/><br />
+					<h2 id="leftSubject">Subject</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="subject"
+					 placeholder="Optional Subject"
+					/><br />
+					<h2 id="leftDate">Dates</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="date"
+					 placeholder="Optional Date"
+					 /><br />
+					<h2 id="leftDescription">Description</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="description"
+					 placeholder="Optional Description"
+					 /><br />
+					<h2 id="leftType">Type</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="type"
+					 placeholder="Optional Type"
+					 /><br />
+					<h2 id="leftFormat">Format</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="format"
+					 placeholder="Optional Format"
+					 /><br />
+					<h2 id="leftLanguage">Language</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="languageBox"
+					 placeholder="Optional Language"
+					 /><br />
+					<h2 id="leftSource">Source</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="sourceBox"
+					 placeholder="Optional Source"
+					 /><br />
+					<h2 id="leftPublisher">Publisher</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="publisher"
+					 placeholder="Optional Publisher"
+					 /><br />
+					<h2 id="leftRights">Rights</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="rights"
+					 placeholder="Optional Rights"
+					 /><br />
+					<h2 id="leftCoverage">Coverage</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="coverage"
+					 placeholder="Optional Coverage"
+					 /><br />
+					<h2 id="leftISBN">ISBN</h2>
+                    <input type="text" 
+					 className="PaperBoxes" 
+					 id="isbn"
+					 placeholder="Optional ISBN"
+					 /><br />
 					<h2 id="leftTags">Tags</h2>
-					<input type="text" className="inputBoxes" id="tags" disabled/><br />
-					<button type="button" className="inputBoxes" id="addTag"><div id="addTagBtnTxt">+</div></button>
-					<input type="text" className="inputBoxes" id="tagsearch" /><br />
+					<input type="text" className="PaperBoxes" id="tags" disabled/><br />
+					<button type="button" 
+					 className="PaperBoxes" 
+					 id="addTag"
+					 onClick={doAddTag}><div id="addTagBtnTxt">+</div></button>
+					<input type="text" className="PaperBoxes" id="tagsearch" /><br />
 
 					<br /><br /><br />
 
@@ -64,7 +185,7 @@ export default class UploadPaper extends React.Component {
 					</form>
 					<div id="uploadStatus"></div>
 
-                    <button type="button" className="inputBoxes" id="upload" onClick={doAddPaper}><div id="uploadBtnTxt">Upload</div></button>
+                    <button type="button" className="PaperBoxes" id="upload" onClick={doAddPaper}><div id="uploadBtnTxt">Upload</div></button>
 
 					<div id="paperStatus"></div>
 
