1a7f91a7692f9e2a2b30f231a6eb06f81bed9d2d
AhmadBarhamje <ahmadbarhamje@hotmail.com>
2021-06-06 15:01:56 -0400

    loads tags and information properly

--- entangled/src/components/EditPaper.js
+++ entangled/src/components/EditPaper.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import './EditPaper.css';
-import { cookies, addPaper, tagExists, addTagToPaper, addMetadataTag } from '../api.js';
+import {
+    cookies, addPaper, tagExists, addTagToPaper,
+    addMetadataTag, getPapersTag, fileURLBase
+} from '../api.js';
 
 let field_ids = ["titleName", "authorBox", "contributor", "relation", "subject", "date",
     "description", "type", "format", "languageBox", "sourceBox",
@@ -18,9 +21,9 @@ let header_ids = ["leftTitle", "leftAuthor", "leftContributor", "leftRelations",
 
 function makeMetadataValues(paperInformation) {
     var curMetadataText = [];
-    for(let index in metadata_ids) {
+    for (let index in metadata_ids) {
         var startValue = "";
-        if(paperInformation[metadata_ids[index]] !== null) {
+        if (paperInformation[metadata_ids[index]] !== null) {
             startValue = paperInformation[metadata_ids[index]];
         }
         curMetadataText.push(startValue);
@@ -28,11 +31,24 @@ function makeMetadataValues(paperInformation) {
     return curMetadataText;
 }
 
+function loadTags(paperInformation) {
+    var userID = cookies.get('UserID');
+    var prefLang = cookies.get('PrefLang');
+    var paperID = paperInformation.id;
+
+    var dict = { userID: userID, language: prefLang, paperID: paperID };
+    var data = getPapersTag(dict);
+
+    return data.tags;
+}
+
 export default class EditPaper extends React.Component {
 
     state = {
         paperInformation: this.props.paperInformation,
-        curMetadataText: makeMetadataValues(this.props.paperInformation)
+        curMetadataText: makeMetadataValues(this.props.paperInformation),
+        currentTags: loadTags(this.props.paperInformation),
+
     }
 
     componentDidMount() {
@@ -42,35 +58,54 @@ export default class EditPaper extends React.Component {
         this.div.appendChild(script);
     }
 
-
     renderRedirect = () => {
         if (cookies.get('UserID') == null) {
             return <Redirect to='/' />
         }
     }
 
+    addTagToList = (tag, tag_id) => {
+        const newTags = this.state.currentTags.slice();
+        newTags.push({ text: tag, tag_id: tag_id });
+        this.setState({ currentTags: newTags });
+    }
+    removeTagFromList = (index) => {
+        const newTags = this.state.currentTags.slice();
+        newTags.splice(index, 1);
+        this.setState({ currentTags: newTags });
+    }
+    findInList = (obj, currentTags) => {
+        var index = -1;
+        for (const x in currentTags) {
+            if (currentTags[x]["text"] === obj["text"] && currentTags[x]["tag_id"] == obj["tag_id"]) {
+                index = x;
+                break;
+            }
+        }
+        return index;
+    }
+
     render() {
-        let tagsList = []
-        let tagIDs = []
+        const { paperInformation, currentTags, curMetadataText } = this.state;
 
         const doAddTag = async e => {
             var tag = document.getElementById("tagsearch").value;
 
             var data = tagExists(tag, cookies.get('PrefLang'), 0);
-            console.log(data);
 
             if (data.tag_id >= 0) {
-                tagsList.push(tag);
-                tagIDs.push(data.tag_id);
+                var obj = { text: tag, tag_id: data.tag_id };
+                var index = this.findInList(obj, currentTags);
+                if (index === -1) {
+                    this.addTagToList(tag, data.tag_id);
+                }
                 document.getElementById("paperStatus").innerHTML = "";
             }
             else {
                 document.getElementById("paperStatus").innerHTML = "Tag Not Found";
             }
-            console.log(tagsList);
 
             document.getElementById("tagsearch").value = '';
-            document.getElementById("tags").value = tagsList.join(", ");
 
             return;
         }
@@ -79,28 +114,22 @@ export default class EditPaper extends React.Component {
             var tag = document.getElementById("tagsearch").value;
 
             var data = tagExists(tag, cookies.get('PrefLang'), 0);
-            console.log(data);
 
             if (data.tag_id >= 0) {
-                let index = tagsList.indexOf(tag);
+                var obj = { text: tag, tag_id: data.tag_id };
+                var index = this.findInList(obj, currentTags);
+
                 if (index > -1) {
-                    tagsList.splice(index, 1);
+                    this.removeTagFromList(index);
                 }
 
-                index = tagIDs.indexOf(data.tag_id);
-                if (index > -1) {
-                    tagIDs.splice(index, 1);
-                }
                 document.getElementById("paperStatus").innerHTML = "";
             }
             else {
                 document.getElementById("paperStatus").innerHTML = "Tag Not Found";
             }
-            console.log(tagsList);
 
             document.getElementById("tagsearch").value = '';
-            document.getElementById("tags").value = tagsList.join(", ");
-
             return;
         }
 
@@ -132,10 +161,10 @@ export default class EditPaper extends React.Component {
             var id = data.id;
 
             var i;
-            for (i = 0; i < tagIDs.length; i++) {
-                data = addTagToPaper(id, tagIDs[i], 0);
-                document.getElementById("paperStatus").innerHTML = data.status;
-            }
+            // for (i = 0; i < tagIDs.length; i++) {
+            //     data = addTagToPaper(id, tagIDs[i], 0);
+            //     document.getElementById("paperStatus").innerHTML = data.status;
+            // }
 
             //add all metadata as tags
             for (const keyVal in metadata_dict) {
@@ -182,7 +211,7 @@ export default class EditPaper extends React.Component {
             document.getElementById("paperStatus").innerHTML = "Uploaded Paper";
         }
 
-        const { paperInformation, curMetadataText } = this.state;
+
         let metadata = [];
 
         const doUpdateArr = async (idx, event) => {
@@ -191,15 +220,17 @@ export default class EditPaper extends React.Component {
             this.setState({ curMetadataText: nxt });
         }
 
-        for(let index in field_ids) {
+        for (let index in field_ids) {
             var curHeader = metadata_categories[index];
             var header = <h2 id={header_ids[index]}>{curHeader}</h2>
-            var input = <input 
-                className="PaperBoxes" 
+            var placeholderValue = "Optional " + metadata_categories[index];
+            var input = <input
+                className="PaperBoxes"
                 id={field_ids[index]}
                 value={curMetadataText[index]}
+                placeholder={placeholderValue}
                 onChange={doUpdateArr.bind(this, index)} />
-            
+
             metadata.push(header);
             metadata.push(input);
         }
@@ -214,12 +245,14 @@ export default class EditPaper extends React.Component {
                     <div className="PaperFields">
                         <div id="MetadataFields">
                             {metadata}
+
                         </div>
                         <hr id="paper_line"></hr>
 
                         <div id="OtherFields">
                             <h2 id="leftTags">Tags</h2>
-                            <input type="text" className="PaperBoxes" id="tags" disabled /><br />
+                            <input type="text" className="PaperBoxes" id="tags" disabled
+                                value={currentTags.map(item => item.text).join(", ")} /><br />
                             <button type="button"
                                 className="PaperBoxes"
                                 id="addTag"
@@ -238,11 +271,8 @@ export default class EditPaper extends React.Component {
                                 <input type="hidden" name="url" id="filename" />
                                 <input type="submit" name="submit" id="paperSubmit" />
                             </form>
-                            <div id="uploadStatus"></div>
 
                             <button type="button" className="PaperBoxes" id="upload" onClick={doAddPaper}><div id="uploadBtnTxt">Upload</div></button>
-
-
 
                         </div>
 
@@ -250,6 +280,11 @@ export default class EditPaper extends React.Component {
                 </div>
                 <div id="paperStatus"></div>
                 <button onClick={this.props.closeEdit} >Go Back</button>
+                {
+                    paperInformation.url !== "none" ?
+                        <a id="currentFile" href={fileURLBase + paperInformation.url} target="_blank" >Current File: {paperInformation.url}</a>
+                        : <></>
+                }
             </body>
         </div>
     }
