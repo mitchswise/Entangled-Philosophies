81c565a0630c5253acae70e197baf6c2239fe93e
Ahmad Barhamje <ahmadbarhamje@hotmail.com>
2021-06-07 10:06:14 -0400

    edit paper now removes existing file if necessary

--- api/editPaper.php
+++ api/editPaper.php
@@ -9,6 +9,27 @@
 	}
 
 	$inData = json_decode(file_get_contents('php://input'), true);
+
+    if($inData["url"] != "none") { //a file is being added
+        $query = "SELECT url FROM papers WHERE id = " . $inData["id"] . ";";
+        $result = $query->conn($query);
+        if(!$result) {
+            echo '{"status":"' . $conn->error . '", "query":"' . $query . '"}';
+            return;
+        }
+        if($result->num_rows == 0) {
+            echo '{"status":"paper does not exist"}';
+            return;
+        }
+
+        $row = $result->fetch_assoc();
+        if($row["url"] != "none") {
+            //need to unlink this file
+            $full_URL = $paperurl . $row["url"];
+            unlink($full_URL);
+        }
+    }
+
     $query = "UPDATE papers SET ";
 
     $metadata_appended = 0;
