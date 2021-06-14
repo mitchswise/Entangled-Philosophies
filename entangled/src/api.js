30eea4338b5bc15da92b439aa7d1afd52db0d43c
AhmadBarhamje <ahmadbarhamje@hotmail.com>
2021-06-14 07:43:10 -0400

    Blaze settings (#36)
    * Settings page api hookup
    
    * added language swapping to working settings page
    
    * changed basename to merge
    
    Co-authored-by: Blaze Wiseman <kawaiimicrowaves930@gmail.com>

--- entangled/src/api.js
+++ entangled/src/api.js
@@ -498,3 +498,17 @@ export function tagFilter(jsonDict) {
 		return null;
 	}
 }
+
+export function updateSettings(id, email, language, cookies) {
+	var jsonPayload = '{"id":' + id + ', "email":"' + email + '", "language":"' + language + '", "cookies":' + cookies + '}';
+	var url = urlBase + '/updateSettings.php';
+	connect("POST", url);
+
+	try {
+		xhr.send(jsonPayload);
+		var jsonObject = JSON.parse(xhr.responseText);
+		return jsonObject;
+	} catch (err) {
+		return null;
+	}
+}
---
+++ entangled/src/dictionary.js
@@ -0,0 +1,56 @@
+export function dSettings(s, lang) {
+	switch(s) {
+		case 1:
+			if (lang == 'eng') return "Username";
+			if (lang == 'ger') return "Benutzername";
+			break;
+		case 2:
+			if (lang == 'eng') return "Email";
+			if (lang == 'ger') return "E-Mail";
+			break;
+		case 3:
+			if (lang == 'eng') return "Password";
+			if (lang == 'ger') return "Passwort";
+			break;
+		case 4:
+			if (lang == 'eng') return "Change Password";
+			if (lang == 'ger') return "Passwort Ändern";
+			break;
+		case 5:
+			if (lang == 'eng') return "Preferred Language";
+			if (lang == 'ger') return "Bevorzugte Sprache";
+			break;
+		case 6:
+			if (lang == 'eng') return "Cookies";
+			if (lang == 'ger') return "Cookies";
+			break;
+		case 7:
+			if (lang == 'eng') return "Opt In";
+			if (lang == 'ger') return "Zustimmen";
+			break;
+		case 8:
+			if (lang == 'eng') return "Opt Out";
+			if (lang == 'ger') return "Aussteigen";
+			break;
+		case 9:
+			if (lang == 'eng') return "Account Settings";
+			if (lang == 'ger') return "Kontoeinstellungen";
+			break;
+		case 10:
+			if (lang == 'eng') return "English";
+			if (lang == 'ger') return "Englisch";
+			break;
+        case 11:
+            if(lang == 'eng') return "Save Changes";
+            if(lang == 'ger') return "Änderungen speichern";
+            break;
+        case 12:
+            if(lang == 'eng') return "Choose Language";
+            if(lang == 'ger') return "Sprache Wählen";
+            break;
+        case 13:
+            if(lang == 'eng') return "German";
+            if(lang == 'ger') return "Deutsche";
+            break;
+	}
+}
