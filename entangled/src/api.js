498ed36d36806c5d434e875144f45cda127f034f
AhmadBarhamje <ahmadbarhamje@hotmail.com>
2021-06-30 11:26:34 -0400

    init commit

--- entangled/src/api.js
+++ entangled/src/api.js
@@ -1,7 +1,8 @@
 import Cookies from 'universal-cookie';
 export const cookies = new Cookies();
 export const supported_languages = ["eng", "ger"];
-export const urlBase = 'http://chdr.cs.ucf.edu/~entangledPhilosophy/Entangled-Philosophies/api';
+// export const urlBase = 'http://chdr.cs.ucf.edu/~entangledPhilosophy/Entangled-Philosophies/api';
+export const urlBase = 'http://chdr.cs.ucf.edu/~ah458967/Entangled-Philosophies/api';
 export const fileURLBase = 'http://chdr.cs.ucf.edu/~entangledPhilosophy/paper/';
 
 var xhr;
